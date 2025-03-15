216fa80474ec243562e2e68f1a9b53cb6d4d0a80
IvanMonichev <risen15@yandex.ru>
2025-03-15 14:32:53 +0300

    feat: add authorization (WIP)

---
+++ frontend/src/features/authorization/authorization.tsx
@@ -0,0 +1,15 @@
+import type { FC } from 'react'
+
+type TProps = {}
+
+const Authorization: FC<TProps> = () => {
+  console.log(`Authorization component is working`)
+
+  return (
+    <div>
+      <div />
+    </div>
+  )
+}
+
+export default Authorization
--- frontend/src/pages/app.router.tsx
+++ frontend/src/pages/app.router.tsx
@@ -1,10 +1,15 @@
 import Home from '@/pages/home/home'
+import SignIn from '@/pages/sign-in/sign-in'
 import { createBrowserRouter, RouteObject } from 'react-router'
 
 const router: RouteObject[] = [
   {
     path: '/',
     element: <Home />
+  },
+  {
+    path: '/sign-in',
+    element: <SignIn />
   }
 ]
 
