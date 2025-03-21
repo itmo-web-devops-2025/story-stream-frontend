57f168560a8d132c01c0e8d7c40fe6b497a6075e
IvanMonichev <risen15@yandex.ru>
2025-03-21 09:20:57 +0300

    feat: add article page

--- frontend/src/features/article/article.tsx
+++ frontend/src/features/article/article.tsx
@@ -1,7 +1,11 @@
-import Text from '@/shared/ui/text/text'
-import CkEditor from '@/shared/widgets/ck-editor/ck-editor'
+import Button from '@/shared/ui/button/button'
+import Form from '@/shared/widgets/form/form'
 import type { Article } from '@/types/article/article.interface'
+import { capitalizeWords } from '@/utils/capitalize-word.util'
 import { dateFormat } from '@/utils/date-format.util'
+import { zodResolver } from '@hookform/resolvers/zod'
+import { useForm } from 'react-hook-form'
+import { z } from 'zod'
 import styles from './article.module.css'
 import 'ckeditor5/ckeditor5.css'
 
@@ -9,31 +13,77 @@ type ArticleProps = {
   article: Article
 }
 
+const schema = z.object({
+  content: z.string()
+})
+
 const Article = ({ article }: ArticleProps) => {
-  console.log(`Article component is working`)
-  console.log(article)
+  const form = useForm({
+    resolver: zodResolver(schema),
+    mode: 'onSubmit'
+  })
 
   return (
     <div className={styles.article}>
       <div className={styles.header}>
         <h3 className={styles.description}>{article.description}</h3>
         <div className={styles.metaBlock}>
-          <p className={styles.date}>{dateFormat(article.createdAt)}</p>
+          <p className={styles.date}>{dateFormat(article.createdAt)} год, </p>
           <p className={styles.timeToRead}>6 мин.</p>
         </div>
-        <p className={styles.author}>{article.author.username}</p>
+        <p className={styles.author}>
+          {capitalizeWords(article.author.username)}
+        </p>
       </div>
 
-      <Text.Paragraph>{article.body}</Text.Paragraph>
+      <div
+        className={styles.body}
+        dangerouslySetInnerHTML={{ __html: article.body }}
+      />
       <div className={styles.comments}>
+        <h3 className={styles.commentsTitle}>Комментарии</h3>
         <ul className={styles.commentsList}>
-          <li className={styles.comment}>Comment 1</li>
-          <li className={styles.comment}>Comment 2</li>
-          <li className={styles.comment}>Comment 3</li>
-          <li className={styles.comment}>Comment 4</li>
+          <li className={styles.comment}>
+            <h4 className={styles.authorComment}>Ivan Ivanov</h4>
+            <p className={styles.commentContent}>
+              Lorem ipsum dolor sit amet, consectetur adipisicing elit. Cumque
+              facilis fugit natus suscipit vel. Aliquam aliquid beatae
+              cupiditate doloribus eaque error perspiciatis, repellat sint
+              ullam!
+            </p>
+          </li>
+          <li className={styles.comment}>
+            <h4 className={styles.authorComment}>Ivan Ivanov</h4>
+            <p className={styles.commentContent}>
+              Lorem ipsum dolor sit amet, consectetur adipisicing elit. Cumque
+              facilis fugit natus suscipit vel. Aliquam aliquid beatae
+              cupiditate doloribus eaque error perspiciatis, repellat sint
+              ullam!
+            </p>
+          </li>
+          <li className={styles.comment}>
+            <h4 className={styles.authorComment}>Denis Gromov</h4>
+            <p className={styles.commentContent}>
+              Lorem ipsum dolor sit amet, consectetur adipisicing elit. Cumque
+              facilis fugit natus suscipit vel. Aliquam aliquid beatae
+              cupiditate doloribus eaque error perspiciatis, repellat sint
+              ullam! Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+              Beatae dolor excepturi expedita, harum magnam molestias natus,
+              nostrum numquam odio officiis omnis, quasi ratione saepe sint
+              tempore. Accusantium blanditiis et nisi.
+            </p>
+          </li>
         </ul>
+        <Form form={form} className={styles.form}>
+          <Form.Item name='content'>
+            <textarea
+              placeholder='Напишите комментарий'
+              className={styles.textArea}
+            />
+          </Form.Item>
+          <Button type='submit'>Отправить</Button>
+        </Form>
       </div>
-      <CkEditor />
     </div>
   )
 }
