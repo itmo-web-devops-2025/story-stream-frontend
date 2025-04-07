3825df4a2d0899284a090f073a1eb6771f1fd610
IvanMonichev <risen15@yandex.ru>
2025-04-07 18:39:32 +0300

    feat: add comments (WIP)

---
+++ frontend/src/features/comment/comment-list.tsx
@@ -0,0 +1,15 @@
+import type { FC } from 'react'
+
+type TProps = {}
+
+const CommentList: FC<TProps> = () => {
+  console.log(`CommentList component is working`)
+
+  return (
+    <div>
+      <div />
+    </div>
+  )
+}
+
+export default CommentList
---
+++ frontend/src/features/comment/component/comments/comments.tsx
@@ -0,0 +1,15 @@
+import type { FC } from 'react'
+
+type TProps = {}
+
+const Comments: FC<TProps> = () => {
+  console.log(`Comments component is working`)
+
+  return (
+    <div>
+      <div />
+    </div>
+  )
+}
+
+export default Comments
