4fd605d57bec6bab0f49f4439d649cd422e90e73
IvanMonichev <risen15@yandex.ru>
2025-04-07 15:57:46 +0300

    feat: add api article list

--- frontend/src/shared/ui/pagination/pagination.tsx
+++ frontend/src/shared/ui/pagination/pagination.tsx
@@ -14,21 +14,56 @@ const Pagination: FC<PaginationProps> = ({
 
   if (totalPages <= 1) return null
 
-  const pages = Array.from({ length: totalPages }, (_, i) => i + 1)
+  // const pages = Array.from({ length: totalPages }, (_, i) => i + 1)
 
+  const generatePages = () => {
+    const pages: (number | string)[] = []
+    const siblingCount = 1 // по одному соседу слева и справа
+
+    const leftSibling = Math.max(currentPage - siblingCount, 2)
+    const rightSibling = Math.min(currentPage + siblingCount, totalPages - 1)
+
+    pages.push(1)
+
+    if (leftSibling > 2) {
+      pages.push('...')
+    }
+
+    for (let i = leftSibling; i <= rightSibling; i++) {
+      pages.push(i)
+    }
+
+    if (rightSibling < totalPages - 1) {
+      pages.push('...')
+    }
+
+    if (totalPages > 1) {
+      pages.push(totalPages)
+    }
+
+    return pages
+  }
+
+  const pages = generatePages()
   return (
     <div className={styles.pagination}>
-      {pages.map((page) => (
-        <button
-          key={page}
-          className={cn(styles.button, {
-            [styles.select]: page === currentPage
-          })}
-          onClick={() => onPageChange(page)}
-        >
-          {page}
-        </button>
-      ))}
+      {pages.map((page, index) =>
+        typeof page === 'number' ? (
+          <button
+            key={page}
+            className={cn(styles.button, {
+              [styles.select]: page === currentPage
+            })}
+            onClick={() => onPageChange(page)}
+          >
+            {page}
+          </button>
+        ) : (
+          <span key={`ellipsis-${index}`} className={styles.ellipsis}>
+            {page}
+          </span>
+        )
+      )}
     </div>
   )
 }
